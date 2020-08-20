8bc40676712ba4ec8b6822bb8ee9561116cdda62
Alexander Popov <alex.popov.tech@gmail.com>
2020-08-20 14:38:37 +0300

    fix environment fox jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  testEnvironment: 'node',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
